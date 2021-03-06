b279a0036a2f05d8baad68f8aa27ca47f1c61e7d
Joemel <bituinjoemel@gmail.com>
2021-03-06 22:09:18 +0800

    [WIP] Expose api modules

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,14 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const {
+  app,
+  contextBridge,
+  ipcRenderer,
+  desktopCapturer,
+  remote,
+} = require('electron');
+const { writeFile } = require('fs');
+const { dialog, Menu } = remote;
+const { buildFromTemplate } = Menu;
+
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('api', {
@@ -15,5 +25,43 @@ contextBridge.exposeInMainWorld('api', {
       // Deliberately strip event as it includes `sender`
       ipcRenderer.on(channel, (event, ...args) => func(...args));
     }
+  },
+  getElectronModules() {
+    return {
+      desktopCapturer,
+    };
+  },
+  getFsModules() {
+    return {
+      writeFile,
+    };
+  },
+  getRemoteModules() {
+    return {
+      dialog,
+      buildFromTemplate,
+      getMenu: () => Menu,
+    };
+  },
+  tryMenu() {
+    console.log('Menu', Menu);
+    const menu = Menu.buildFromTemplate([
+      {
+        label: 'Joms',
+        submenu: [
+          { role: 'about' },
+          { type: 'separator' },
+          { role: 'services' },
+          { type: 'separator' },
+          { role: 'hide' },
+          { role: 'hideothers' },
+          { role: 'unhide' },
+          { type: 'separator' },
+          { role: 'quit' },
+        ],
+      },
+    ]);
+    Menu.setApplicationMenu(menu);
+    menu.popup();
   },
 });
