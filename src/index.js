78d9ccf9fc91511b679c9a0ffded15ad0bac4aca
Joemel <bituinjoemel@gmail.com>
2021-03-09 00:49:02 +0800

    Add minimum window dimensions

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 500,
+    minHeight: 300,
     webPreferences: {
       contextIsolation: true,
       enableRemoteModule: true,
